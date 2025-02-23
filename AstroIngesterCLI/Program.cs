1894056fed82f72d4fcc32f8f5155c7c03b34141
Woofer21 <61364477+Woofer21@users.noreply.github.com>
2025-02-23 11:18:49 -0500

    Update gitignore & start on extracting metadata.

--- AstroIngesterCLI/Program.cs
+++ AstroIngesterCLI/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
+using System.IO;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
 
 namespace AstroIngesterCLI
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] files = System.IO.Directory.GetFiles("E:/Testing");
+
+			foreach (string file in files)
+			{
+				Console.WriteLine($"Found File: {file}");
+
+                string comment = getComment(file);
+                if (string.IsNullOrEmpty(comment))
+                {
+                    Console.WriteLine("Unable to get comment on image. Either No comment present or it errored");
+                } else Console.WriteLine(comment);
+            }
+
+			Console.WriteLine("Ended");
+            Console.ReadLine();
+		}
+
+		static string getComment(string filePath)
+		{
+			try
+			{ 
+                IEnumerable<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(filePath);
+
+                foreach (MetadataExtractor.Directory directory in directories)
+                {
+                    foreach (Tag tag in directory.Tags)
+                        if (tag.Type == ExifDirectoryBase.TagUserComment)
+                            return tag.Description;
+                }
+
+                return null;
+            } catch (Exception e)
+			{
+                Console.WriteLine("Unable to get comment: " + e);
+				return null;
+			}
+
         }
     }
 }
