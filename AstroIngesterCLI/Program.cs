9b54b8413e24ef0436460f6fa68355950fbc3eb7
Woofer21 <61364477+Woofer21@users.noreply.github.com>
2025-03-01 10:41:11 -0500

    feat: add drive select

--- AstroIngesterCLI/Program.cs
+++ AstroIngesterCLI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using AstroIngesterCore;
 
 namespace AstroIngesterCLI
@@ -7,24 +9,59 @@ namespace AstroIngesterCLI
 	{
 		static void Main(string[] args)
 		{
-			string[] files = System.IO.Directory.GetFiles("E:/Testing");
 
-			foreach (string file in files)
+			FileTools fileT = new();
+			DriveInfo[] drives = fileT.GetDrives();
+			DriveInfo selectedDrive = null;
+
+			bool gettingDrive = true;
+
+			while (gettingDrive)
 			{
-				try
+                Console.WriteLine("Type a number to select an 'Input Drive' from the list below");
+                
+				for (int i = 0; i < drives.Length; i++)
                 {
-                    DateTime picDate = MetadataTools.GetDate(file);
-                    Console.WriteLine($"{file} - {picDate}");
-
-                    string picComment = MetadataTools.GetComment(file);
-					if (string.IsNullOrEmpty(picComment)) Console.WriteLine($"{file} - No Comment Added");
-					else Console.WriteLine($"{file} - {picComment}");
+                    Console.WriteLine($"{i + 1}) {drives[i].Name}");
                 }
-				catch (Exception e)
+                Console.WriteLine("-1) Close");
+
+                Console.Write("Drive Number: ");
+				string drive = Console.ReadLine();
+                bool isValidDrive = int.TryParse(drive, out int driveNum);
+
+				if (driveNum == -1) Process.GetCurrentProcess().Kill();
+
+				if (isValidDrive)
 				{
-					Console.WriteLine("Unhandled Main: " + e.Message);
-				}
-			}
+                    bool isDriveSelected = fileT.SelectDrive(driveNum, out selectedDrive);
+                    if (isDriveSelected) gettingDrive = false;
+                }
+            }
+
+            Console.WriteLine($"Selected Drive {selectedDrive.Name}");
+
+
+
+
+			//string[] files = System.IO.Directory.GetFiles("E:/Testing");
+
+			//foreach (string file in files)
+			//{
+			//	try
+   //             {
+   //                 DateTime picDate = MetadataTools.GetDate(file);
+   //                 Console.WriteLine($"{file} - {picDate}");
+
+   //                 string picComment = MetadataTools.GetComment(file);
+			//		if (string.IsNullOrEmpty(picComment)) Console.WriteLine($"{file} - No Comment Added");
+			//		else Console.WriteLine($"{file} - {picComment}");
+   //             }
+			//	catch (Exception e)
+			//	{
+			//		Console.WriteLine("Unhandled Main: " + e.Message);
+			//	}
+			//}
 
 			Console.WriteLine("Ended");
             Console.ReadLine();
---
+++ AstroIngesterCore/FileTools.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroIngesterCore
+{
+    public class FileTools
+    {
+        private DriveInfo[] drives = DriveInfo.GetDrives();
+        private DriveInfo selectedDrive;
+
+        public DriveInfo[] GetDrives()
+        {
+            return drives;
+        }
+
+        public bool SelectDrive(int driveNum, out DriveInfo selected)
+        {
+            if (driveNum - 1 > drives.Length - 1 || driveNum < 1)
+            {
+                selected = null;
+                return false;
+            }
+
+            selectedDrive = drives[driveNum - 1];
+            selected = selectedDrive;
+            return true;
+        }
+    }
+}
